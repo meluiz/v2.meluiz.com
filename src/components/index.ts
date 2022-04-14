9b759724be7bf0bb24e31c6576797a19b831d7c5
meluiz <me.luizfelipe@gmail.com>
2022-04-14 18:02:58 -0300

    feat: layout componente created

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
+export { default as Layout } from './layout'
 export { default as Navigation } from './navigation'
---
+++ src/components/layout/index.tsx
@@ -0,0 +1,24 @@
+import type { LayoutProps } from '@src/types/components/layout'
+import React from 'react'
+
+import { Navigation } from '@src/components'
+import { Box } from '@chakra-ui/react'
+
+const Component = ({ children }: LayoutProps) => {
+  return (
+    <Box
+      id="__app"
+      w="100vw"
+      h="100vh"
+      position="relative"
+      overflow="auto"
+      bg="mauve.1200"
+      color="mauve.100"
+    >
+      <Navigation />
+      {children}
+    </Box>
+  )
+}
+
+export default Component
