fe31d980c305dc2ab81f0f90f7084878be0c2a3f
meluiz <me.luizfelipe@gmail.com>
2022-04-15 15:18:03 -0300

    styles: adding mauve light colors

--- src/theme/chakra.theme.ts
+++ src/theme/chakra.theme.ts
@@ -1,5 +1,5 @@
 import { extendTheme, ThemeConfig } from '@chakra-ui/react'
-import { mauveDark } from '@radix-ui/colors'
+import { mauveDark, mauve } from '@radix-ui/colors'
 
 const config: ThemeConfig = {
   initialColorMode: 'dark',
@@ -28,6 +28,20 @@ const Theme = extendTheme(
         1000: mauveDark.mauve3,
         1100: mauveDark.mauve2,
         1200: mauveDark.mauve1,
+      },
+      mauve_light: {
+        100: mauve.mauve12,
+        200: mauve.mauve11,
+        300: mauve.mauve10,
+        400: mauve.mauve9,
+        500: mauve.mauve8,
+        600: mauve.mauve7,
+        700: mauve.mauve6,
+        800: mauve.mauve5,
+        900: mauve.mauve4,
+        1000: mauve.mauve3,
+        1100: mauve.mauve2,
+        1200: mauve.mauve1,
       },
     },
     components: {
