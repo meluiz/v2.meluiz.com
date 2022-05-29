4f9e6bcb00829e2aa5d75bd05d6c5c1d512666d6
meluiz <me.luizfelipe@gmail.com>
2022-05-28 23:00:10 -0300

    feat: adding effect on heading and some buttons

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,5 +1,5 @@
 import type { LayoutProps } from '@src/types/components/layout'
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 import { Navigation } from '@src/components'
 import { Box, useColorModeValue } from '@chakra-ui/react'
@@ -7,11 +7,13 @@ import { Box, useColorModeValue } from '@chakra-ui/react'
 const Component = ({ children }: LayoutProps) => {
   const innerCursor = useRef<HTMLDivElement>(null)
   const outerCursor = useRef<HTMLDivElement>(null)
+  const [isFixed, updateFixed] = useState(false)
 
   function mousemove(event: MouseEvent) {
     const inner = innerCursor.current
     const outer = outerCursor.current
-    if (inner && outer) {
+
+    if (inner && outer && !isFixed) {
       const __x = event.clientX
       const __y = event.clientY
 
@@ -43,17 +45,97 @@ const Component = ({ children }: LayoutProps) => {
     }
   }
 
+  function cursorScalableEnter() {
+    const inner = innerCursor.current
+    const outer = outerCursor.current
+
+    if (inner && outer) {
+      inner.style.transform = `translate(-50%, -50%) scale(12)`
+      inner.style.mixBlendMode = `exclusion`
+      outer.style.transform = `translate(-50%, -50%) scale(0)`
+    }
+  }
+
+  function cursorScalableLeave() {
+    const inner = innerCursor.current
+    const outer = outerCursor.current
+
+    if (inner && outer) {
+      inner.style.transform = `translate(-50%, -50%) scale(1)`
+      inner.style.mixBlendMode = `unset`
+      outer.style.transform = `translate(-50%, -50%) scale(1)`
+    }
+  }
+
+  function huggedButtonMove(event: MouseEvent) {
+    const inner = innerCursor.current
+    const outer = outerCursor.current
+
+    if (inner && outer) {
+      const target = event.currentTarget as HTMLButtonElement
+      const { width, height, top, left } = target.getBoundingClientRect()
+      const __br = getComputedStyle(target).borderRadius
+
+      updateFixed(true)
+
+      inner.style.transform = `translate(-50%, -50%) scale(0)`
+      outer.style.transform = `translate(0, 0) scale(1)`
+      outer.style.width = `${width}px`
+      outer.style.height = `${height}px`
+      outer.style.top = `${top}px`
+      outer.style.left = `${left}px`
+      outer.style.borderRadius = `${__br}`
+    }
+  }
+
+  function huggedButtonLeave() {
+    const inner = innerCursor.current
+    const outer = outerCursor.current
+
+    updateFixed(false)
+
+    if (inner && outer) {
+      inner.style.transform = `translate(-50%, -50%) scale(1)`
+      outer.style.transform = `translate(-50%, -50%) scale(1)`
+      outer.style.width = `28px`
+      outer.style.height = `28px`
+      outer.style.borderRadius = `28px`
+    }
+  }
+
   useEffect(() => {
     document.addEventListener('mousemove', mousemove, false)
     document.addEventListener('mouseleave', mouseleave, false)
     document.addEventListener('mouseenter', mouseenter, false)
 
+    const headings = document.querySelectorAll('h1,h2,h3')
+    for (const heading of Array.from(headings)) {
+      heading.addEventListener('mouseenter', cursorScalableEnter, false)
+      heading.addEventListener('mouseleave', cursorScalableLeave, false)
+    }
+
+    const buttons = document.querySelectorAll('button')
+    for (const button of Array.from(buttons)) {
+      button.addEventListener('mousemove', huggedButtonMove, false)
+      button.addEventListener('mouseleave', huggedButtonLeave, false)
+    }
+
     return () => {
       document.removeEventListener('mousemove', mousemove, false)
       document.removeEventListener('mouseleave', mouseleave, false)
       document.removeEventListener('mouseenter', mouseenter, false)
+
+      for (const heading of Array.from(headings)) {
+        heading.removeEventListener('mouseover', cursorScalableEnter, false)
+        heading.removeEventListener('mouseleave', cursorScalableLeave, false)
+      }
+
+      for (const button of Array.from(buttons)) {
+        button.removeEventListener('mouseenter', huggedButtonMove, false)
+        button.removeEventListener('mouseleave', huggedButtonLeave, false)
+      }
     }
-  }, [])
+  }, [isFixed])
 
   return (
     <Box
@@ -94,7 +176,7 @@ const Component = ({ children }: LayoutProps) => {
           lg: 'block',
         }}
         border="1px solid #BF4E63"
-        borderRadius="9999px"
+        borderRadius="28px"
         pos="fixed"
         top="-50%"
         left="-50%"
